473fe093c4b7050b1c183dfdcea7ddc235559860
rakib hasan <dev.rakibhasan1@gmail.com>
2025-07-21 01:36:41 +0600

    initially setup Next.Js app

---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,33 @@
+"use client"
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+export default function Navbar() {
+    const pathname = usePathname();
+    console.log(pathname, pathname.includes("dashboard"));
+    if (!pathname.includes("dashboard")) {
+        return <div>
+            <nav>
+                <ul className="h-18 flex justify-center items-center gap-6">
+                    <Link href="/">
+                        <li className="border px-3 py-2">Home</li>
+                    </Link>
+                    <Link href="/services">
+                        <li className="border px-3 py-2">Services</li>
+                    </Link>
+                    <Link href="/about">
+                        <li className="border px-3 py-2">About</li>
+                    </Link>
+
+                    {/* <li className="border px-3 py-2">About</li>
+          <li className="border px-3 py-2">Contact Us</li>
+          <li className="border px-3 py-2">Dashboard</li> */}
+                </ul>
+            </nav>
+        </div>
+    }
+    else {
+        return <></>
+    }
+};
