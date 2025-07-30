039bc350da55fd332235ef3f84aaa1f74585237e
rakib hasan <dev.rakibhasan1@gmail.com>
2025-07-31 00:02:53 +0600

    explore CRUD functionality

--- src/app/api/items/[id]/route.js
+++ src/app/api/items/[id]/route.js
@@ -1,23 +1,34 @@
-export async function GET(req, params) {
+import { dbConnect } from "@/lib/dbConnect";
+import { ObjectId } from "mongodb";
 
-    const id = await params;
+const client = await dbConnect();
+const db = client.db("NextJs");
+const collection = db.collection("textNextJs");
+
+export async function GET(req, { params }) {
+
+    const id = await params.id;
     console.log(id);
+    const singleData = await collection.findOne({ _id: new ObjectId(id) })
 
-    return Response.json({ params: id })
+    return Response.json(singleData)
 };
 
-export async function DELETE(req, params) {
+export async function DELETE(req, { params }) {
 
-    const id = await params;
+    const id = await params.id;
     console.log(id);
+    const response = await collection.deleteOne({ _id: new ObjectId(id) })
 
-    return Response.json({ params: id })
+    return Response.json(response)
 };
 
-export async function PATCH(req, params) {
+export async function PATCH(req, { params }) {
 
-    const id = await params;
+    const id = await params.id;
     console.log(id);
-
-    return Response.json({ params: id })
+    const postedData = await req.json();
+    const filter = { _id: new ObjectId(id) };
+    const updatedResponse = await collection.updateOne(filter, { $set: { ...postedData } }, { upsert: true });
+    return Response.json(updatedResponse);
 };
